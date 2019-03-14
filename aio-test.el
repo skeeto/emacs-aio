b08240538d3bc2331cad7bf8c8db760e095aa813
Christopher Wellons <wellons@nullprogram.com>
2019-03-13 21:47:27 -0400

    Add aio-with-test macro for unit tests (with timeout)

--- aio-test.el
+++ aio-test.el
@@ -14,43 +14,54 @@
 (require 'cl-lib)
 (require 'ert)
 
+(defmacro aio-with-test (timeout &rest body)
+  "Run body asynchronously but block synchronously until it completes.
+
+If TIMEOUT seconds passes without completion, signal an
+aio-timeout to cause the test to fail."
+  (declare (indent 1))
+  `(let* ((promises (list (aio-with-async ,@body)
+                          (aio-timeout ,timeout)))
+          (select (aio-make-select promises)))
+     (aio-wait-for
+      (aio-with-async
+        (aio-await (aio-await (aio-select select)))))))
+
 ;; Tests:
 
 (ert-deftest sleep ()
-  (aio-wait-for
-   (aio-with-async
-     (let ((start (float-time)))
-       (dotimes (i 3)
-         (should (eql i(aio-await (aio-sleep 0.5 i)))))
-       (should (> (- (float-time) start)
-                  1.4))))))
+  (aio-with-test 3
+    (let ((start (float-time)))
+      (dotimes (i 3)
+        (should (eql i
+                     (aio-await (aio-sleep 0.5 i)))))
+      (should (> (- (float-time) start)
+                 1.4)))))
 
 (ert-deftest repeat ()
-  (aio-wait-for
-   (aio-with-async
-     (let ((sub (aio-lambda (result) (aio-await (aio-sleep .1 result)))))
-       (should (eq :a (aio-await (funcall sub :a))))
-       (should (eq :b (aio-await (funcall sub :b))))))))
+  (aio-with-test 3
+    (let ((sub (aio-lambda (result) (aio-await (aio-sleep .1 result)))))
+      (should (eq :a (aio-await (funcall sub :a))))
+      (should (eq :b (aio-await (funcall sub :b)))))))
 
 (ert-deftest timeout ()
-  (aio-wait-for
-   (aio-with-async
-     (let ((sleep (aio-sleep 1.0 t))
-           (timeout (aio-timeout 0.5))
-           (select (aio-make-select)))
-       (aio-select-add select sleep)
-       (aio-select-add select timeout)
-       (let ((winner (aio-await (aio-select select))))
-         (should (equal '(:error aio-timeout . 0.5)
-                        (aio-await (aio-catch winner))))))
-     (let ((sleep (aio-sleep 0.1 t))
-           (timeout (aio-timeout 0.5))
-           (select (aio-make-select)))
-       (aio-select-add select sleep)
-       (aio-select-add select timeout)
-       (let ((winner (aio-await (aio-select select))))
-         (should (equal '(:success . t)
-                        (aio-await (aio-catch winner)))))))))
+  (aio-with-test 4
+    (let ((sleep (aio-sleep 1.0 t))
+          (timeout (aio-timeout 0.5))
+          (select (aio-make-select)))
+      (aio-select-add select sleep)
+      (aio-select-add select timeout)
+      (let ((winner (aio-await (aio-select select))))
+        (should (equal '(:error aio-timeout . 0.5)
+                       (aio-await (aio-catch winner))))))
+    (let ((sleep (aio-sleep 0.1 t))
+          (timeout (aio-timeout 0.5))
+          (select (aio-make-select)))
+      (aio-select-add select sleep)
+      (aio-select-add select timeout)
+      (let ((winner (aio-await (aio-select select))))
+        (should (equal '(:success . t)
+                       (aio-await (aio-catch winner))))))))
 
 (defun aio-test--shuffle (values)
   "Return a shuffled copy of VALUES."
@@ -61,47 +72,44 @@
              finally return (append v nil))))
 
 (ert-deftest sleep-sort ()
-  (aio-wait-for
-   (aio-with-async
-     (let* ((values (cl-loop for i from 5 to 60
-                             collect (/ i 20.0) into values
-                             finally return (aio-test--shuffle values)))
-            (count (length values))
-            (select (aio-make-select))
-            (promises (dolist (value values)
-                        (aio-select-add select (aio-sleep value value))))
-            (last 0.0))
-       (dotimes (_ count :done)
-         (let ((promise (aio-await (aio-select select))))
-           (let ((result (aio-await promise)))
-             (should (> result last))
-             (setf last result))))))))
+  (aio-with-test 8
+    (let* ((values (cl-loop for i from 5 to 60
+                            collect (/ i 20.0) into values
+                            finally return (aio-test--shuffle values)))
+           (count (length values))
+           (select (aio-make-select))
+           (promises (dolist (value values)
+                       (aio-select-add select (aio-sleep value value))))
+           (last 0.0))
+      (dotimes (_ count :done)
+        (let ((promise (aio-await (aio-select select))))
+          (let ((result (aio-await promise)))
+            (should (> result last))
+            (setf last result)))))))
 
 (ert-deftest process-sentinel ()
-  (aio-wait-for
-   (aio-with-async
-     (let ((process (start-process-shell-command "test" nil "exit 0"))
-           (sentinel (aio-make-callback)))
-       (setf (process-sentinel process) (car sentinel))
-       (should (equal "finished\n"
-                      (nth 1 (aio-chain (cdr sentinel)))))))))
+  (aio-with-test 10
+    (let ((process (start-process-shell-command "test" nil "exit 0"))
+          (sentinel (aio-make-callback)))
+      (setf (process-sentinel process) (car sentinel))
+      (should (equal "finished\n"
+                     (nth 1 (aio-chain (cdr sentinel))))))))
 
 (ert-deftest process-filter ()
-  (aio-wait-for
-   (aio-with-async
-     (let* ((command
-             (if (eq system-type 'windows-nt)
-                 (mapconcat #'identity
-                            '("echo a b c"
-                              "waitfor /t 1 x 2>nul"
-                              "echo 1 2 3"
-                              "waitfor /t 1 x 2>nul")
-                            "&")
-               "echo a b c; sleep 1; echo 1 2 3; sleep 1"))
-            (process (start-process-shell-command "test" nil command))
-            (filter (aio-make-callback)))
-       (setf (process-filter process) (car filter))
-       (should (equal "a b c\n"
-                      (nth 1 (aio-chain (cdr filter)))))
-       (should (equal "1 2 3\n"
-                      (nth 1 (aio-chain (cdr filter)))))))))
+  (aio-with-test 10
+    (let* ((command
+            (if (eq system-type 'windows-nt)
+                (mapconcat #'identity
+                           '("echo a b c"
+                             "waitfor /t 1 x 2>nul"
+                             "echo 1 2 3"
+                             "waitfor /t 1 x 2>nul")
+                           "&")
+              "echo a b c; sleep 1; echo 1 2 3; sleep 1"))
+           (process (start-process-shell-command "test" nil command))
+           (filter (aio-make-callback)))
+      (setf (process-filter process) (car filter))
+      (should (equal "a b c\n"
+                     (nth 1 (aio-chain (cdr filter)))))
+      (should (equal "1 2 3\n"
+                     (nth 1 (aio-chain (cdr filter))))))))
