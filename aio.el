077722896e649e7a33dcafbc4585686a29423979
Christopher Wellons <wellons@nullprogram.com>
2019-04-20 17:45:31 -0400

    Bump to 1.0

--- aio.el
+++ aio.el
@@ -4,7 +4,7 @@
 
 ;; Author: Christopher Wellons <wellons@nullprogram.com>
 ;; URL: https://github.com/skeeto/emacs-aio
-;; Version: 0.1
+;; Version: 1.0
 ;; Package-Requires: ((emacs "26.1"))
 
 ;;; Commentary:
